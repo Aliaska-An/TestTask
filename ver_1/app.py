09ed235e3942848429dbba1837a639917228d09b
Aliaska-An <anastasia_alyasova@mail.ru>
2021-08-27 23:42:56 +0300

    ver_1

---
+++ ver_1/app.py
@@ -0,0 +1,113 @@
+from flask import Flask, request, json, Response
+from flask_pymongo import PyMongo
+from bson import json_util
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config['MONGO_URI']='mongodb://localhost/shop'
+mongo = PyMongo(app)
+
+# entities.py
+class Products(object):
+    def __init__(self, name, description, parameters):
+        #        self.id = id
+        self.name = name
+        self.description = description
+        self.parameters = parameters
+
+class ProductsParameters(object):
+    def __init__(self, productionYear, country, color):
+        #        self.id = id
+        self.productionYear = productionYear
+        self.country = country
+        self.color = color
+
+
+'''@patients_add.route('/addPatient', methods=['POST'])
+def add_patient():
+    req = request.form
+    if collection.count({'phone_number': req['phone_number']}) > 0:
+        return Response(status=302)
+    else:
+        product = Products(name=req['name'], description=req['description'],
+                          date_of_birth=datetime.strptime(req['date'], '%Y-%m-%d'),
+                          country=req['country'], city=req['city'])
+        print(patient.__dict__)
+        collection.insert_one(patient.__dict__)
+    return Response(status=200)'''
+
+# Добавление в коллекцию элемента
+@app.route('/products/addProduct', methods=['POST'])
+def addProduct():
+    name = request.json['name']
+    description = request.json['description']
+    parameters = request.json['parameters']
+
+
+    if name and description and parameters:
+        id = mongo.db.products.insert(
+            {'name': name, 'description': description,
+             'parameters': parameters })
+        response = {
+            'id': str(id),
+            'name': name,
+            'description': description,
+            'parameters': parameters
+        }
+        return response
+    else:
+        {'message': 'received'}
+    return {'message': 'received'}
+
+
+# Получить все товары
+@app.route('/products/getAllProducts', methods=['GET'])
+def getAllProducts():
+    products = mongo.db.products.find()
+    response = json_util.dumps(products)
+
+    return Response(response, mimetype='application/json')
+
+
+# Получить товар по ID
+@app.route('/products/getProductID/<id>', methods=['GET'])
+def getProduct_ID(id):
+    product = mongo.db.products.find_one({'_id': ObjectId(id)})
+    response = json_util.dumps(product)
+    return Response(response, mimetype='application/json')
+
+
+# Получить список названий товаров
+@app.route('/products/getAllProductsNames', methods=['GET'])
+def getAllProductsNames():
+    docs_list = list(mongo.db.products.find({}, {"name": 1, "_id": 0}))
+    return {"products": json.loads(json.dumps(docs_list, default=json_util.default))}
+
+# Получить товар по NAME
+@app.route('/products/getProductsName/<name>', methods=['GET'])
+def getProducts_NAME(name):
+    docs_list = list(mongo.db.products.find({}, {"_id": 0, "name": name}))
+    return {"products": json.loads(json.dumps(docs_list, default=json_util.default))}
+
+
+# Получить товар по YEAR
+@app.route('/products/getProductsYear/<year>', methods=['GET'])
+def getProducts_YEAR(year):
+    docs_list = list(mongo.db.products.find({"parameters.year": int(year)}, {"_id": 0, "parameters.year": 1, "name": 1}))
+    return {"products": json.loads(json.dumps(docs_list, default=json_util.default))}
+
+# Получить товар по COUNTRY
+@app.route('/products/getProductsCountry/<country>', methods=['GET'])
+def getProducts_COUNTRY(country):
+    docs_list = list(mongo.db.products.find({"parameters.country": country}, {"_id": 0, "name": 1}))
+    return {"products": json.loads(json.dumps(docs_list, default=json_util.default))}
+
+# Получить товар по COUNTRY
+@app.route('/products/getProductsColor/<color>', methods=['GET'])
+def getProducts_COLOR(color):
+    docs_list = list(mongo.db.products.find({"parameters.color": color}, {"_id": 0, "name": 1}))
+    return {"products": json.loads(json.dumps(docs_list, default=json_util.default))}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
